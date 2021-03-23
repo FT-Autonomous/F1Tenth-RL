1ae053982d72722aa6d6cd181db65281211429f4
eoin <avantgardadev@gmail.com>
2021-03-23 22:45:50 +0000

    Add comments and finish example script

---
+++ simple-example.py
@@ -0,0 +1,75 @@
+"""
+Sample python script to show the process of choosing a Stable Baselines model,
+training it with a chosen policy, and then evaluating the trained model on the
+environment while visualising it
+"""
+
+import gym
+import time
+import numpy as np
+
+from datetime import datetime
+
+from stable_baselines3 import A2C
+from wrappers import F110_Wrapped
+
+
+### TRAIN ###
+
+TRAIN_STEPS = pow(10, 3)
+
+# create environment
+env = gym.make('f110_gym:f110-v0', map='./f1tenth_gym/examples/example_map', map_ext='.png', num_agents=1)
+
+# wrap (modify) the environment to meet SB3 requirements
+env = F110_Wrapped(env)
+
+# choose RL model and policy here
+model = A2C('MlpPolicy', env, verbose=1)
+
+# train model and record time taken
+start_time = time.time()
+model.learn(total_timesteps=TRAIN_STEPS) # takes about one second per 500 training steps
+print(f"Training time {time.time() - start_time:.2f}s")
+print('Training cycle complete.')
+
+# save model with unique timestamp
+timestamp = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+model.save(f"./train/a2c-f110-{timestamp}")
+
+
+### EVALUATE ###
+
+MIN_EVAL_EPISODES = 5
+
+# create evaluation environment (same as train environment in this case)
+eval_env = gym.make('f110_gym:f110-v0', map='./f1tenth_gym/examples/example_map', map_ext='.png', num_agents=1)
+
+# wrap evaluation environment
+eval_env = F110_Wrapped(eval_env)
+
+# simulate a few episodes and render them, ctrl-c to cancel an episode
+episode = 0
+while episode < MIN_EVAL_EPISODES:
+    try:
+        episode += 1
+        obs = eval_env.reset()
+        done = False
+        while not done:
+            # use trained model to predict some action, based off the observations
+            action, _ = model.predict(obs)
+            obs, _, done, _ = eval_env.step(action)
+            eval_env.render()
+        # this section just asks the user if they want to run more episodes
+        if episode == (MIN_EVAL_EPISODES - 1):
+            choice = input('Another episode? (Y/N) ')
+            if choice.replace(" ", "").lower() in ['y', 'yes']:
+                episode -= 1
+            else:
+                episode = MIN_EVAL_EPISODES
+    except KeyboardInterrupt:
+        pass
+
+# close the environments (not sure if necessary)
+env.close()
+eval_env.close()
--- wrappers.py
+++ wrappers.py
@@ -4,14 +4,21 @@ import numpy as np
 from gym import spaces
 
 class F110_Wrapped(gym.Wrapper):
+  """
+  This is a wrapper for the F1Tenth Gym environment intended
+  for only one car, but should be expanded to handle multi-agent scenarios
+  """
 
   def __init__(self, env):
     super().__init__(env)
+
     # action space, steer and speed
     self.action_space = spaces.Box(low=np.array([-1.0, -1.0]), high=np.array([1.0, 1.0]), dtype=np.float)
+    
     # observations, just take the lidar scans
     self.observation_space = spaces.Box(low=0.0, high=30.0, shape=(1080,), dtype=np.float)
-    # get action details for action scaling
+    
+    # save steering/speed allowed ranges for normalising actions (for RL algorithms)
     self.s_min = self.env.params['s_min']
     self.s_max = self.env.params['s_max']
     self.v_min = self.env.params['v_min']
@@ -24,20 +31,31 @@ class F110_Wrapped(gym.Wrapper):
     action_convert = self.convert_actions(action)
     observation, _, done, info = self.env.step(np.array([action_convert]))
     
-    # TODO -> do some reward engineering here
+    ### REWARD ###
+
+    # TODO -> do some reward engineering here and mess around with this
+    # currently setting the speed of the car to be a positive reward
     vel_magnitude = np.linalg.norm([observation['linear_vels_x'][0], observation['linear_vels_y'][0]])
     reward = vel_magnitude
+    # but if the car crashes, then assign a large negative reward
     if observation['collisions'][0]:
       reward = -450
+    
+    # the reward below could be a possible sparse reward
     # reward = self.env.lap_counts[0]
 
     return observation['scans'][0], reward, bool(done), info
 
   def reset(self):
-    observation, _, _, _ = self.env.reset(np.array([[self.env.start_xs[0], self.env.start_ys[0], self.env.start_thetas[0]]]))
-    return observation['scans'][0]  # reward, done, info can't be included
+    # should start off in slightly different positions to help with training
+    rand_x = np.random.uniform(-1.0, 1.0)
+    rand_y = np.random.uniform(-1.0, 1.0)
+    rand_t = np.random.uniform(65.0, 125.0)
+    observation, _, _, _ = self.env.reset(np.array([[rand_x, rand_y, np.radians(rand_t)]]))
+    return observation['scans'][0]  # reward, done, info can't be included in the Gym format
 
   def convert_actions(self, actions):
+    # convert actions from range [-1, 1] to the normal steering/speed range
     steer = (((actions[0] + 1) * self.range_s) / 2) + self.s_min
     speed = (((actions[1] + 1) * self.range_v) / 2) + self.v_min
     return np.array([steer, speed], dtype=np.float)
